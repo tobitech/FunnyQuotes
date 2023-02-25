57ae7345dbf0187fd0b844835b6153051f0937d3
Tobi Omotayo <tobitech@ymail.com>
2023-02-25 01:32:55 +0100

    Initial Commit

---
+++ FunnyQuotes/FunnyQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyQuotesApp.swift
+//  FunnyQuotes
+//
+//  Created by Oluwatobi Omotayo on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FunnyQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
